6c105d37d3712aaad71ca5f0a9f6890501215682
Carlos Sosa <carloseduardososagonzalez@gmail.com>
2024-04-11 18:33:18 -0600

    Avance en las paginas

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import home, products, register, exit
+from .views import home, nosotros, products, register, exit, perfil, publicar, Ventas_Publicadas
 
 
 urlpatterns = [
     path('', home, name='home'),
+    path('nosotros/',nosotros, name='nosotros'),
+    path('perfil/',perfil, name='perfil'),
+    path('Ventas_Publicadas/',Ventas_Publicadas, name='Ventas_Publicadas'),
+    path('publicar/',publicar, name='publicar'),
     path('products/', products, name='products'),
     path('register/', register, name='register'),
     path('logout/', exit, name='exit'),
